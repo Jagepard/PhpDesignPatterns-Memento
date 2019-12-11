4ca7b6dc08c61228140c13dcf0e1008e0e370155
Jagepard <dankorot@gmail.com>
2019-12-11 14:23:52 +0300

    Update docblock

--- src/CaretakerInterface.php
+++ src/CaretakerInterface.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @license   https://mit-license.org/ MIT
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
  */
 
 namespace Behavioral\Memento;
